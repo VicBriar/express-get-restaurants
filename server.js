d762e95836d50f32a60c7896b94e4df1934266e1
Victoria McCrady <spartanginger@gmail.com>
2023-03-06 12:10:40 -0800

    server.js now returns all restaurants

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const {sequelize} = require("./db");
 
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
 
+//TOFIX: Create your GET Request Route Below: 
+app.get("/restaurants", async (request, response) => {
+    let restaurants = await Restaurant.findAll();
+    response.send(restaurants)
+})
 
 app.listen(port, () => {
     sequelize.sync();
