7ab4c1d61ff44712523fffb2611ca8809049da13
Victoria McCrady <spartanginger@gmail.com>
2023-03-08 08:08:46 -0800

    refactor
    removed unecessary require from express that imported response and request objects.

--- server.js
+++ server.js
@@ -2,14 +2,25 @@ const express = require("express");
 const app = express();
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
+const { Musician } = require("../../Database/musicians-bands/Models/Musician");
 
 const port = 3000;
 
+app.use(express.json({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
-//TOFIX: Create your GET Request Route Below: 
 app.get("/restaurants/:id", async (request, response) => {
     let restaurant = await Restaurant.findByPk(request.params.id);
     response.json(restaurant)
+})
+app.post('/restaurants', async (request,response) => {
+    try {
+        let restaurant = request.body;
+        await Musician.create(restaurant)
+        response.send("Musician added!")
+    }catch(err){
+        console.log(err)
+    }
 })
 
 app.listen(port, () => {
