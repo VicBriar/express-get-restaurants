ccc1b6606a46a53dfdaa55f429ffe1d41e4c084f
Victoria McCrady <spartanginger@gmail.com>
2023-03-08 15:53:08 -0800

    feat:
    post request to /restaurants validates name, location and cuisine

--- routers/restaurant.js
+++ routers/restaurant.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();;
 const {Restaurant} = require("../models/index");
 const {sequelize} = require('../db');
+const {body,check,validationResult} = require('express-validator');
 
 
 router.get('/', async (request,response) =>{
@@ -15,14 +16,28 @@ router.get("/:id", async (request, response) => {
     response.json(restaurant)
 })
 
-router.post('/', async (request,response) => {
-    try {
-        let restaurant = request.body;
-        await Restaurant.create(restaurant)
-        response.send("restaurant added!")
-    }catch(err){
-        console.log(err)
-    }
+router.post(
+    '/',
+    [
+        check("name").not().isEmpty().trim().withMessage("name cannot be empty"),
+        check("location").not().isEmpty().trim().withMessage("location cannot be empty"),
+        check("cuisine").not().isEmpty().trim().withMessage("cuisine cannot be empty")
+    ],
+    async (request,response) => {
+        const errors  = validationResult(request);
+        if(errors.isEmpty()){
+            try {
+                let restaurant = request.body;
+                await Restaurant.create(restaurant)
+                let restaurants = await Restaurant.findAll()
+                response.json(restaurants)
+            }catch(err){
+                console.log(err)
+            }
+        } else {
+            response.status(400).json({errors: errors.array()})
+            console.log("errors were found")
+        }
 })
 
 router.put('/:id', async (request,response) => {
